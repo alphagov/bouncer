c9f0c0c14a98fca901849e4225f6cce743815250
Tom Stuart <tom@codon.com>
2013-07-05 17:28:35 +0100

    Implement Mapping.create

--- lib/mapping.rb
+++ lib/mapping.rb
@@ -1,2 +1,5 @@
 class Mapping
+  def self.create(*args)
+    new *args
+  end
 end
--- spec/units/mapping_spec.rb
+++ spec/units/mapping_spec.rb
@@ -3,4 +3,19 @@ require 'spec_helper'
 require 'mapping'
 
 describe Mapping do
+  describe '.create' do
+    let(:attributes) { double 'attributes' }
+    let(:mapping) { double 'mapping' }
+
+    before(:each) do
+      Mapping.stub new: mapping
+    end
+
+    specify { Mapping.create(attributes).should == mapping }
+
+    specify do
+      Mapping.should_receive(:new).with(attributes)
+      Mapping.create(attributes)
+    end
+  end
 end
