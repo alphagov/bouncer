b80e81c7225e598fcb85723be58c798ae7a22d19
Tom Stuart <tom@codon.com>
2013-07-05 12:45:00 +0100

    Drive out basic implementation of Bouncer#call
    This assumes that Host and Mapping support the Active Record 4.0 query
    interface, which is likely to be the case once they're implemented.

--- lib/bouncer.rb
+++ lib/bouncer.rb
@@ -1,5 +1,16 @@
+require 'rack'
+require 'digest/sha1'
+
 class Bouncer
   def call(env)
-    [200, {}, []]
+    request = Rack::Request.new(env)
+    host = Host.find_by host: request.host
+    path_hash = Digest::SHA1.hexdigest(request.path)
+    mapping = host.site.mappings.find_by path_hash: path_hash
+
+    case mapping.http_status
+    when '410'
+      [410, {}, []]
+    end
   end
 end
--- spec/bouncer_spec.rb
+++ spec/bouncer_spec.rb
@@ -7,8 +7,64 @@ describe Bouncer do
   include Rack::Test::Methods
 
   let(:app) { subject }
+  let(:host_class) { double 'Host' }
+  let(:host) { double 'host' }
+  let(:hostname) { 'example.com' }
+  let(:path) { '/' }
+  let(:url) { "http://#{hostname}#{path}" }
+  let(:path_hash) { double }
+  let(:site) { double 'site' }
+  let(:mappings) { double 'mappings' }
+  let(:mapping) { double 'mapping' }
+  let(:status_code) { 410 }
+
+  before(:each) do
+    stub_const 'Host', host_class
+
+    host_class.stub find_by: host
+    Digest::SHA1.stub hexdigest: path_hash
+    host.stub site: site
+    site.stub mappings: mappings
+    mappings.stub find_by: mapping
+    mapping.stub http_status: status_code.to_s
+  end
 
   it 'should respond to requests' do
-    expect { get '/' }.to_not raise_error
+    expect { get url }.to_not raise_error
+  end
+
+  it 'should find the right host' do
+    host_class.should_receive(:find_by).with(host: hostname)
+    get url
+  end
+
+  it 'should hash the path' do
+    Digest::SHA1.should_receive(:hexdigest).with(path)
+    get url
+  end
+
+  it 'should get the host\'s site' do
+    host.should_receive(:site).with(no_args)
+    get url
+  end
+
+  it 'should get the site\'s mappings' do
+    site.should_receive(:mappings).with(no_args)
+    get url
+  end
+
+  it 'should find the right mapping' do
+    mappings.should_receive(:find_by).with(path_hash: path_hash)
+    get url
+  end
+
+  it 'should get the mapping\'s status code' do
+    mapping.should_receive(:http_status).with(no_args)
+    get url
+  end
+
+  it 'should respond with the correct status code' do
+    get url
+    last_response.status.should == status_code
   end
 end
