91b39c6113b9de1507ad04ddfc4bbf31903c0ae1
Tom Stuart <tom@codon.com>
2013-07-05 17:39:14 +0100

    Implement Mapping constructor

--- lib/mapping.rb
+++ lib/mapping.rb
@@ -1,5 +1,9 @@
-class Mapping
+class Mapping < Struct.new(:path, :http_status, :new_url)
   def self.create(*args)
     new *args
+  end
+
+  def initialize(attributes)
+    super *attributes.values_at(*members)
   end
 end
--- spec/units/mapping_spec.rb
+++ spec/units/mapping_spec.rb
@@ -18,4 +18,17 @@ describe Mapping do
       Mapping.create(attributes)
     end
   end
+
+  describe '.new' do
+    let(:path) { double 'path' }
+    let(:http_status) { double 'HTTP status' }
+    let(:new_url) { double 'new URL' }
+
+    subject { Mapping.new path: path, http_status: http_status, new_url: new_url }
+
+    it { should be_a Mapping }
+    its(:path) { should == path }
+    its(:http_status) { should == http_status }
+    its(:new_url) { should == new_url }
+  end
 end
