1134459513497bd811faf9065de42531a3e689de
Tom Stuart <tom@codon.com>
2013-07-22 15:23:59 +0100

    Introduce intermediate contexts for when host and path are recognised

--- spec/units/bouncer_spec.rb
+++ spec/units/bouncer_spec.rb
@@ -7,23 +7,13 @@ describe Bouncer do
   include Rack::Test::Methods
 
   let(:app) { subject }
-  let(:host) { double 'host' }
   let(:hostname) { 'example.com' }
   let(:path) { '/' }
   let(:url) { "http://#{hostname}#{path}" }
-  let(:path_hash) { double 'path hash' }
-  let(:site) { double 'site' }
-  let(:mappings) { double 'mappings' }
-  let(:mapping) { double 'mapping' }
 
   before(:each) do
     stub_const 'Host', double
     Host.stub find_by: host
-    Digest::SHA1.stub hexdigest: path_hash
-    host.stub site: site
-    site.stub mappings: mappings
-    mappings.stub find_by: mapping
-    mapping.stub http_status: status_code.to_s
   end
 
   shared_examples 'a redirector' do
@@ -42,73 +32,95 @@ describe Bouncer do
     end
   end
 
-  shared_examples 'a redirector which recognises the host' do
-    it_should_behave_like 'a redirector'
-
-    it 'should hash the path' do
-      Digest::SHA1.should_receive(:hexdigest).with(path)
-      get url
-    end
-
-    it 'should get the host\'s site' do
-      host.should_receive(:site).with(no_args)
-      get url
-    end
-
-    it 'should get the site\'s mappings' do
-      site.should_receive(:mappings).with(no_args)
-      get url
-    end
-
-    it 'should try to find the right mapping' do
-      mappings.should_receive(:find_by).with(path_hash: path_hash)
-      get url
-    end
-  end
-
-  shared_examples 'a redirector which recognises the host and path' do
-    it_should_behave_like 'a redirector which recognises the host'
-
-    it 'should get the mapping\'s status code' do
-      mapping.should_receive(:http_status).with(no_args)
-      get url
-    end
-  end
-
-  context 'when the URL has been redirected' do
-    let(:status_code) { 301 }
-    let(:new_url) { double 'new URL' }
+  context 'when the host is recognised' do
+    let(:host) { double 'host' }
+    let(:path_hash) { double 'path hash' }
+    let(:site) { double 'site' }
+    let(:mappings) { double 'mappings' }
 
     before(:each) do
-      mapping.stub new_url: new_url
+      Digest::SHA1.stub hexdigest: path_hash
+      host.stub site: site
+      site.stub mappings: mappings
+      mappings.stub find_by: mapping
     end
 
-    it_should_behave_like 'a redirector which recognises the host and path'
+    shared_examples 'a redirector which recognises the host' do
+      it_should_behave_like 'a redirector'
 
-    it 'should get the mapping\'s new URL' do
-      mapping.should_receive(:new_url).with(no_args)
-      get url
+      it 'should hash the path' do
+        Digest::SHA1.should_receive(:hexdigest).with(path)
+        get url
+      end
+
+      it 'should get the host\'s site' do
+        host.should_receive(:site).with(no_args)
+        get url
+      end
+
+      it 'should get the site\'s mappings' do
+        site.should_receive(:mappings).with(no_args)
+        get url
+      end
+
+      it 'should try to find the right mapping' do
+        mappings.should_receive(:find_by).with(path_hash: path_hash)
+        get url
+      end
     end
 
-    it 'should respond with a redirect' do
-      get url
-      last_response.should be_redirect
-    end
+    context 'when the path is recognised' do
+      let(:mapping) { double 'mapping' }
 
-    it 'should redirect to the new URL' do
-      get url
-      last_response.location.should == new_url
-    end
-  end
+      before(:each) do
+        mapping.stub http_status: status_code.to_s
+      end
 
-  context 'when the URL has been archived' do
-    let(:status_code) { 410 }
+      shared_examples 'a redirector which recognises the host and path' do
+        it_should_behave_like 'a redirector which recognises the host'
 
-    it_should_behave_like 'a redirector which recognises the host and path'
+        it 'should get the mapping\'s status code' do
+          mapping.should_receive(:http_status).with(no_args)
+          get url
+        end
+      end
 
-    it 'should respond with a client error' do
-      get url
-      last_response.should be_client_error
+      context 'when the URL has been redirected' do
+        let(:status_code) { 301 }
+        let(:new_url) { double 'new URL' }
+
+        before(:each) do
+          mapping.stub new_url: new_url
+        end
+
+        it_should_behave_like 'a redirector which recognises the host and path'
+
+        it 'should get the mapping\'s new URL' do
+          mapping.should_receive(:new_url).with(no_args)
+          get url
+        end
+
+        it 'should respond with a redirect' do
+          get url
+          last_response.should be_redirect
+        end
+
+        it 'should redirect to the new URL' do
+          get url
+          last_response.location.should == new_url
+        end
+      end
+
+      context 'when the URL has been archived' do
+        let(:status_code) { 410 }
+
+        it_should_behave_like 'a redirector which recognises the host and path'
+
+        it 'should respond with a client error' do
+          get url
+          last_response.should be_client_error
+        end
+      end
     end
   end
 end
