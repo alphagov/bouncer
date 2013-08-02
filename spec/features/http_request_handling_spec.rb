505b8612b546af0ea274e200f24e186de3aff1ab
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-08-02 15:49:51 +0100

    Move matchers into spec/support

--- spec/features/http_request_handling_spec.rb
+++ spec/features/http_request_handling_spec.rb
@@ -193,30 +193,3 @@ describe 'HTTP request handling' do
     last_response.location.should == 'http://www.gov.uk/government/organisations/ministry-of-truth'
   end
 end
-
-RSpec::Matchers.define :be_valid_xml do
-  match do |string|
-    begin
-      Nokogiri::XML::Document.parse(string) { |config| config.strict }
-      true
-    rescue Nokogiri::XML::SyntaxError
-      false
-    end
-  end
-end
-
-RSpec::Matchers.define :be_valid_sitemap do
-  match do |string|
-    sitemap = Nokogiri::XML::Document.parse(string)
-    schema = Nokogiri::XML::Schema.new(File.read(File.expand_path('../sitemap.xsd', __FILE__)))
-
-    schema.valid?(sitemap)
-  end
-end
-
-RSpec::Matchers.define :have_sitemap_entry_for do |url|
-  match do |string|
-    sitemap = Nokogiri::XML::Document.parse(string)
-    !sitemap.xpath("/xmlns:urlset/xmlns:url/xmlns:loc[text()='#{url}']").empty?
-  end
-end
