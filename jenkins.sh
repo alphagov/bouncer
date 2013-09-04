683151eb39fdb2b1b21b53393a39e23685a61dad
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-09-04 14:58:16 +0000

    Use the correct user

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
-mysql -u root -e 'DROP DATABASE transition_test'
-mysql -u root -e 'CREATE DATABASE transition_test'
+mysql -u transition -ptransition -e 'DROP DATABASE transition_test'
+mysql -u transition -ptransition -e 'CREATE DATABASE transition_test'
 mysql -u transition -ptransition -D transition_test < db/structure.sql
 
 RACK_ENV=test bundle exec rake --trace
