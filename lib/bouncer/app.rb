e4cedafbd225b50c06ec72f9bf828321fa98efa6
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-01-08 12:03:31 +0000

    Simplify app.rb's main logic to single case
    Reads a little better than multiple elsif's and a nested case

--- lib/bouncer/app.rb
+++ lib/bouncer/app.rb
@@ -7,25 +7,24 @@ module Bouncer
     def call(env)
       context = RequestContext.new(CanonicalizedRequest.new(env))
 
-      outcome = if context.host.nil?
-        case context.request.path
-        when '/healthcheck' then Outcome::Healthcheck
-        else
-          Outcome::UnrecognisedHost
-        end
-      elsif ['/404', '/410'].include?(context.request.path)
-        Outcome::TestThe4xxPages
-      elsif context.request.path == '/sitemap.xml'
-        Outcome::Sitemap
-      elsif context.request.path == '/robots.txt'
-        Outcome::Robots
-      elsif context.site.global_http_status
-        Outcome::GlobalHTTPStatus
-      elsif context.request.path == '' # after c14n, '' is equivalent to '/'
-        Outcome::Homepage
-      else
-        Outcome::Status
-      end
+      outcome = case
+                when context.host.nil? && context.request.path == '/healthcheck'
+                  Outcome::Healthcheck
+                when context.host.nil?
+                  Outcome::UnrecognisedHost
+                when ['/404', '/410'].include?(context.request.path)
+                  Outcome::TestThe4xxPages
+                when context.request.path == '/sitemap.xml'
+                  Outcome::Sitemap
+                when context.request.path == '/robots.txt'
+                  Outcome::Robots
+                when context.site.global_http_status
+                  Outcome::GlobalHTTPStatus
+                when context.request.path == '' # after c14n, '' is equivalent to '/'
+                  Outcome::Homepage
+                else
+                  Outcome::Status
+                end
 
       outcome.new(context, @renderer).serve
     end
