8d63718f473e7961c1ad2d2cd96088a9790f1053
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-01-24 12:46:25 +0000

    Refactor rules

--- lib/bouncer/rules.rb
+++ lib/bouncer/rules.rb
@@ -2,23 +2,31 @@ module Bouncer
   ##
   # Regex rules to try after mappings but just before we 404
   class Rules
+    def self.redirect(location)
+      [301, { 'Location' => location }, []]
+    end
+
+    def self.gone(context, renderer)
+      [410, { 'Content-Type' => 'text/html' }, [renderer.render(context, '410')]]
+    end
+
     def self.try(context, renderer)
       request = context.request
       if request.host == 'www.dfid.gov.uk' && request.path =~ %r{^/r4d/(.*)$}
         new_path = request.non_canonicalised_path.gsub(%r{^/r4d/}, "")
-        [301, { 'Location' => "http://r4d.dfid.gov.uk/#{new_path}" }, []]
+        redirect("http://r4d.dfid.gov.uk/#{new_path}")
       elsif request.host == 'www.dh.gov.uk' && request.path =~ %r{/dh_digitalassets/}
-        [410, { 'Content-Type' => 'text/html' }, [renderer.render(context, '410')]]
+        gone(context, renderer)
       elsif request.host == 'www.direct.gov.uk' && request.path =~ %r{/(en/)?AdvancedSearch}i
-        [301, { 'Location' => 'https://www.gov.uk/search' }, []]
+        redirect('https://www.gov.uk/search')
       elsif request.host == 'campaigns.direct.gov.uk' && request.path =~ %r{/firekills}
-        [301, { 'Location' => 'https://www.gov.uk/firekills' }, []]
+        redirect('https://www.gov.uk/firekills')
       elsif request.host == 'www.number10.gov.uk' && request.path =~ %r{^/news/?([_0-9a-zA-Z-]+)?/([0-9]+)/([0-9]+)/(.*)-([0-9]+)$}
-        [301, { 'Location' => "http://www.number10.gov.uk/news/#{$4}" }, []]
+        redirect("http://www.number10.gov.uk/news/#{$4}")
       elsif request.host == 'cdn.hm-treasury.gov.uk' && request.path =~ %r{^/(.*)$}
-        [301, { 'Location' => "http://www.hm-treasury.gov.uk/#{$1}" }, []]
+        redirect("http://www.hm-treasury.gov.uk/#{$1}")
       elsif request.host == 'digital.cabinetoffice.gov.uk' && request.path =~ %r{^/(.*)$}
-        [301, { 'Location' => "https://gds.blog.gov.uk/#{$1}" }, []]
+        redirect("https://gds.blog.gov.uk/#{$1}")
       end
     end
   end
