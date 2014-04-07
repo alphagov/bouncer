14fc23e75a25becbb601f075f6af3d4be213b72c
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-04-07 10:35:08 +0000

    Make EA rules case insensitive

--- lib/bouncer/preemptive_rules.rb
+++ lib/bouncer/preemptive_rules.rb
@@ -10,9 +10,9 @@ module Bouncer
     def self.try(context, renderer)
       request = context.request
       if request.host == 'www.environment-agency.gov.uk'
-        if request.non_canonicalised_fullpath =~ %r{^/homeandleisure/floods/riverlevels/(.*)$}
+        if request.non_canonicalised_fullpath =~ %r{^/homeandleisure/floods/riverlevels/(.*)$}i
           redirect("http://apps.environment-agency.gov.uk/river-and-sea-levels/#{$1}")
-        elsif request.non_canonicalised_fullpath =~ %r{^/homeandleisure/floods/((cy/)?(34678|34681|147053)\.aspx(\?.*)?)$}
+        elsif request.non_canonicalised_fullpath =~ %r{^/homeandleisure/floods/((cy/)?(34678|34681|147053)\.aspx(\?.*)?)$}i
           redirect("http://apps.environment-agency.gov.uk/flood/#{$1}")
         end
       end
