c3e4f3bfb8f90ed27ed4d7fe17dae0fd780fc7df
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-05-29 13:43:33 +0000

    Remove references to Mapping.http_status
    This field has been removed from Mapping, so we need to stop setting it.

--- spec/units/mapping_spec.rb
+++ spec/units/mapping_spec.rb
@@ -20,15 +20,15 @@ describe Mapping do
   describe '.new' do
     let(:path) { double 'path' }
     let(:path_hash) { double 'path hash' }
-    let(:http_status) { double 'HTTP status' }
+    let(:type) { double 'type' }
     let(:new_url) { double 'new URL' }
 
-    subject { Mapping.new path: path, path_hash: path_hash, http_status: http_status, new_url: new_url }
+    subject { Mapping.new path: path, path_hash: path_hash, type: type, new_url: new_url }
 
     it { should be_a Mapping }
     its(:path) { should == path }
     its(:path_hash) { should == path_hash }
-    its(:http_status) { should == http_status }
+    its(:type) { should == type }
     its(:new_url) { should == new_url }
   end
 end
