13bcb8ade7b3a10b5f46d05248d4eed5c3828e2b
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-10-30 10:44:34 +0000

    Rudimentary rake-based benchmarking
    Gives us a simple base from which to make perf-related changes. At time of
    commit, default 1000-run benchmark results were:
    
    vagrant@development:/var/govuk/bouncer$ bundle exec rake benchmark:all
                  user     system      total        real
    no host:  1.020000   0.080000   1.100000 (  1.434709)
    410:      0.690000   0.050000   0.740000 (  1.013773)
    301:      0.700000   0.040000   0.740000 (  1.024336)
    404:      0.750000   0.000000   0.750000 (  1.045783)

---
+++ lib/benchmark/all.rb
@@ -0,0 +1,35 @@
+require 'rack/test'
+require 'benchmark'
+
+module Benchmark
+  ##
+  # Tests Bouncer using Rack::Test with a combination of host not found/
+  # path not found/archive/redirects, defaulting to 1000 requests each.
+  #
+  # Not to be used for production performance testing - this is in-memory
+  # and serves only as a basis for making relative code improvements.
+  #
+  class All
+    include Rack::Test::Methods
+
+    DEFAULT_NUMBER_OF_RUNS = 1000
+
+    attr_accessor :number_of_runs
+    def initialize(number_of_runs)
+      self.number_of_runs = (number_of_runs || DEFAULT_NUMBER_OF_RUNS).to_i
+    end
+
+    def app
+      @app ||= Rack::Builder.parse_file('config.ru')[0]
+    end
+
+    def run!
+      Benchmark.bm(7) do |x|
+        x.report("no host:") { number_of_runs.times { get 'http://www.fluffy.gov.uk/news'     } }
+        x.report("410:")     { number_of_runs.times { get 'http://www.direct.gov.uk/news'     } }
+        x.report("301:")     { number_of_runs.times { get 'http://www.direct.gov.uk/motoring' } }
+        x.report("404:")     { number_of_runs.times { get 'http://www.direct.gov.uk/prickly'  } }
+      end
+    end
+  end
+end
