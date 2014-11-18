955383ca18b978c9ed616a4b792b912e02a211ff
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-11-18 16:26:52 +0000

    Check if there are conflicts on master when running the build
    - This is taken from
      https://github.com/alphagov/specialist-publisher/commit/9a097249c142c6e7146706dfd5fc0282c92f4886,
      as a thing discussed in tech lead standup that could be useful and
      passed to me to do via Jamie.

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -x
+
+# Try to merge master into the current branch, and abort if it doesn't exit
+# cleanly (ie there are conflicts). This will be a noop if the current branch
+# is master.
+git merge --no-commit origin/master || git merge --abort
+
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
 dropdb transition_test
