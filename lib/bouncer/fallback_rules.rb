36693482ea20d1e6d5888edfadaf92f90b9502b3
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-12-15 17:08:59 +0000

    For completeness, add non-www support to the fallback rules too
    - Only duplicate one test case per non-www rule.

--- lib/bouncer/fallback_rules.rb
+++ lib/bouncer/fallback_rules.rb
@@ -12,16 +12,16 @@ module Bouncer
 
     def self.try(context, renderer)
       request = context.request
-      if request.host == 'www.dfid.gov.uk' && request.path =~ %r{^/r4d/(.*)$}
+      if (request.host == 'www.dfid.gov.uk' || request.host == 'dfid.gov.uk') && request.path =~ %r{^/r4d/(.*)$}
         new_path = request.non_canonicalised_path.gsub(%r{^/r4d/}, "")
         redirect("http://r4d.dfid.gov.uk/#{new_path}")
-      elsif request.host == 'www.dh.gov.uk' && request.path =~ %r{/dh_digitalassets/}
+      elsif (request.host == 'www.dh.gov.uk' || request.host == 'dh.gov.uk') && request.path =~ %r{/dh_digitalassets/}
         gone(context, renderer)
-      elsif request.host == 'www.direct.gov.uk' && request.path =~ %r{/(en/)?AdvancedSearch}i
+      elsif (request.host == 'www.direct.gov.uk' || request.host == 'direct.gov.uk') && request.path =~ %r{/(en/)?AdvancedSearch}i
         redirect('https://www.gov.uk/search')
       elsif request.host == 'campaigns.direct.gov.uk' && request.path =~ %r{/firekills}
         redirect('https://www.gov.uk/firekills')
-      elsif request.host == 'www.number10.gov.uk' && request.path =~ %r{^/news/?([_0-9a-zA-Z-]+)?/([0-9]+)/([0-9]+)/(.*)-([0-9]+)$}
+      elsif (request.host == 'www.number10.gov.uk' || request.host == 'number10.gov.uk') && request.path =~ %r{^/news/?([_0-9a-zA-Z-]+)?/([0-9]+)/([0-9]+)/(.*)-([0-9]+)$}
         redirect("http://www.number10.gov.uk/news/#{$4}")
       elsif request.host == 'cdn.hm-treasury.gov.uk' && request.path =~ %r{^/(.*)$}
         redirect("http://www.hm-treasury.gov.uk/#{$1}")
