ddde55f1787cbe0eceece9050d5f1fd4ad363339
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-12-16 09:56:13 +0000

    Reformat fallback rules
    * From `if`/`elsif` to nested `case`
    * Consistent with preemptive rules

--- lib/bouncer/fallback_rules.rb
+++ lib/bouncer/fallback_rules.rb
@@ -12,27 +12,35 @@ module Bouncer
 
     def self.try(context, renderer)
       request = context.request
-      if (request.host == 'www.dfid.gov.uk' || request.host == 'dfid.gov.uk') && request.path =~ %r{^/r4d/(.*)$}
-        new_path = request.non_canonicalised_path.gsub(%r{^/r4d/}, "")
-        redirect("http://r4d.dfid.gov.uk/#{new_path}")
-      elsif (request.host == 'www.dh.gov.uk' || request.host == 'dh.gov.uk') && request.path =~ %r{/dh_digitalassets/}
-        gone(context, renderer)
-      elsif (request.host == 'www.direct.gov.uk' || request.host == 'direct.gov.uk') && request.path =~ %r{/(en/)?AdvancedSearch}i
-        redirect('https://www.gov.uk/search')
-      elsif request.host == 'campaigns.direct.gov.uk' && request.path =~ %r{/firekills}
-        redirect('https://www.gov.uk/firekills')
-      elsif (request.host == 'www.number10.gov.uk' || request.host == 'number10.gov.uk') && request.path =~ %r{^/news/?([_0-9a-zA-Z-]+)?/([0-9]+)/([0-9]+)/(.*)-([0-9]+)$}
-        redirect("http://www.number10.gov.uk/news/#{$4}")
-      elsif request.host == 'cdn.hm-treasury.gov.uk' && request.path =~ %r{^/(.*)$}
-        redirect("http://www.hm-treasury.gov.uk/#{$1}")
-      elsif request.host == 'digital.cabinetoffice.gov.uk' && request.path =~ %r{^/(.*)$}
-        redirect("https://gds.blog.gov.uk/#{$1}")
-      elsif request.host == 'govstore.service.gov.uk' && request.path =~ %r{^/cloudstore/([_0-9a-zA-Z-]+)$}
-        redirect("http://www.digitalmarketplace.service.gov.uk/service/#{$1}")
-      elsif request.host == 'govstore.service.gov.uk' && request.path =~ %r{^/cloudstore(/[ips]aas|/scs)(/[_0-9a-zA-Z-]+){0,2}/([_0-9a-zA-Z-]+)$}
-        redirect("http://www.digitalmarketplace.service.gov.uk/service/#{$3}")
-      elsif request.host == 'govstore.service.gov.uk'
-        redirect("https://www.gov.uk/digital-marketplace")
+
+      case request.host
+      when 'www.dfid.gov.uk', 'dfid.gov.uk'
+        if request.path =~ %r{^/r4d/(.*)$}
+          new_path = request.non_canonicalised_path.gsub(%r{^/r4d/}, '')
+          redirect("http://r4d.dfid.gov.uk/#{new_path}")
+        end
+      when 'www.dh.gov.uk', 'dh.gov.uk'
+        gone(context, renderer) if request.path =~ %r{/dh_digitalassets/}
+      when 'www.direct.gov.uk', 'direct.gov.uk'
+        redirect('https://www.gov.uk/search') if request.path =~ %r{/(en/)?AdvancedSearch}i
+      when 'campaigns.direct.gov.uk'
+        redirect('https://www.gov.uk/firekills') if request.path =~ %r{/firekills}
+      when 'www.number10.gov.uk', 'number10.gov.uk'
+        redirect("http://www.number10.gov.uk/news/#{$4}") if
+          request.path =~ %r{^/news/?([_0-9a-zA-Z-]+)?/([0-9]+)/([0-9]+)/(.*)-([0-9]+)$}
+      when 'cdn.hm-treasury.gov.uk'
+        redirect("http://www.hm-treasury.gov.uk/#{$1}") if request.path =~ %r{^/(.*)$}
+      when 'digital.cabinetoffice.gov.uk'
+        redirect("https://gds.blog.gov.uk/#{$1}") if request.path =~ %r{^/(.*)$}
+      when 'govstore.service.gov.uk'
+        case request.path
+        when %r{^/cloudstore/([_0-9a-zA-Z-]+)$}
+          redirect("http://www.digitalmarketplace.service.gov.uk/service/#{$1}")
+        when %r{^/cloudstore(/[ips]aas|/scs)(/[_0-9a-zA-Z-]+){0,2}/([_0-9a-zA-Z-]+)$}
+          redirect("http://www.digitalmarketplace.service.gov.uk/service/#{$3}")
+        else
+          redirect('https://www.gov.uk/digital-marketplace')
+        end
       end
     end
   end
