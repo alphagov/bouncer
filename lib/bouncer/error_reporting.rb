66ab98b0100e795830f9633bf3d2e092fc0d29d7
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-06-10 15:08:12 +0100

    Bump govuk_app_config to version 4.0.0.pre.4
    This should resolve the datasync-related errors in Sentry we're
    seeing for Bouncer. No other [manual migration steps](https://docs.sentry.io/platforms/ruby/migration/)
    look necessary, apart from swapping out `Raven::Rack` for
    `Sentry::Rack::CaptureExceptions`.
    
    I've removed the `lib/bouncer/error_reporting.rb` file as all it
    did was exclude `PG::ConnectionBad` exceptions, happening "during
    data syncs":
    https://github.com/alphagov/bouncer/blob/eb7462e6bb8667ab0b3cd37ff90a32288e697b5a/lib/bouncer/error_reporting.rb#L2-L4
    
    We now have dedicated `data_sync_excluded_exceptions` logic for
    ignoring errors ONLY when they occur during the data sync, which
    looks to be what was wanted in the original case here.

--- lib/bouncer/error_reporting.rb
+++
@@ -1,5 +0,0 @@
-Raven.configure do |config|
-  # In staging and integration we often see tens of thousands `PG::ConnectionBad`
-  # errors a day during data syncs. This is eating up our Sentry quota.
-  config.excluded_exceptions << "PG::ConnectionBad"
-end
